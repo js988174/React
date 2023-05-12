7f26d52a792014ea79cfc797479b5552d0138119
js988174 <76925402+js988174@users.noreply.github.com>
2023-05-12 18:08:39 +0900

    208

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Header from './component/Header';
+import AboveGame from './component/AboveGame';
+import Game from './component/Game';
+import useLocalStorageNumber from './hook/useLocalStorageNumber';
 
-function App() {
-    return ( <
-        div className = "App" > < /div>
-    );
+export default function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useLocalStorageNumber('bestScore', 0);
+
+  useEffect(() => {
+    if (score > bestScore) {
+      setBestScore(score);
+    }
+  });
+
+  return (
+    <div className="container">
+      <Header score={score} bestScore={bestScore} />
+      <AboveGame />
+      <Game setScore={setScore} />
+    </div>
+  );
 }
-
-
-export default App;
